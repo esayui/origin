ce2af2f38dbff783a63fd99aef62f209b5572305
ChangMing Han <han.changming@qq.com>
2018-08-22 16:48:33 +0800

    Initial commit

---
+++ src/main/java/com/rengu/operationsmanagementsuitev3/OperationsManagementSuiteV3Application.java
@@ -0,0 +1,12 @@
+package com.rengu.operationsmanagementsuitev3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperationsManagementSuiteV3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OperationsManagementSuiteV3Application.class, args);
+    }
+}
