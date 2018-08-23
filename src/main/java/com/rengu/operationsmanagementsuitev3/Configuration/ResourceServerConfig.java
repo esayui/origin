7fd50423bc45e7bfd39d2b65034a04d0a81a690b
韩常铭 <han.changming@qq.com>
2018-08-23 18:18:18 +0800

    修复跨域请求异常的问题

--- src/main/java/com/rengu/operationsmanagementsuitev3/Configuration/ResourceServerConfig.java
+++ src/main/java/com/rengu/operationsmanagementsuitev3/Configuration/ResourceServerConfig.java
@@ -9,6 +9,9 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 /**
  * @program: OperationsManagementSuiteV3
@@ -33,14 +36,30 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
         return jwtAccessTokenConverter;
     }
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+    }
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.cors();
+        // 放行所有Option请求
+        http.authorizeRequests().antMatchers(HttpMethod.OPTIONS).permitAll();
         // 放行用户注册接口
         http.authorizeRequests().antMatchers(HttpMethod.POST, "/users/user").permitAll();
         // 放行swagger2文档页面
         http.authorizeRequests().antMatchers("/swagger-ui.html", "/webjars/**", "/swagger-resources/**", "/v2/api-docs").permitAll();
+        // 放行测试接口
+        http.authorizeRequests().antMatchers("/uploads/**").permitAll();
         http.authorizeRequests().anyRequest().authenticated();
     }
 }
