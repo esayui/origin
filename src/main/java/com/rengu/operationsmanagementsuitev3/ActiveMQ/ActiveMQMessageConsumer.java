a96b778223e19f1a0ba4946a952a43c3da51426e
韩常铭 <han.changming@qq.com>
2018-08-27 11:19:09 +0800

    实现消息消费者

---
+++ src/main/java/com/rengu/operationsmanagementsuitev3/ActiveMQ/ActiveMQMessageConsumer.java
@@ -0,0 +1,21 @@
+package com.rengu.operationsmanagementsuitev3.ActiveMQ;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Service;
+
+/**
+ * @program: operations-management-suite-v3
+ * @author: hanch
+ * @create: 2018-08-27 11:10
+ **/
+
+@Slf4j
+@Service
+public class ActiveMQMessageConsumer {
+
+    @JmsListener(destination = "temp.queue")
+    public void receiveQueue(String message) {
+        log.info("ActiveMQ:" + message);
+    }
+}
