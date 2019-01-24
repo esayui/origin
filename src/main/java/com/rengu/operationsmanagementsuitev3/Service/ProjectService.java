8161364f445918130a4f76df45187b134ee1a3d9
韩常铭 <han.changming@qq.com>
2019-01-24 09:44:49 +0800

    移除复制工程接口

--- src/main/java/com/rengu/operationsmanagementsuitev3/Service/ProjectService.java
+++ src/main/java/com/rengu/operationsmanagementsuitev3/Service/ProjectService.java
@@ -5,7 +5,6 @@ import com.rengu.operationsmanagementsuitev3.Entity.UserEntity;
 import com.rengu.operationsmanagementsuitev3.Repository.ProjectRepository;
 import com.rengu.operationsmanagementsuitev3.Utils.ApplicationMessages;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
@@ -55,21 +54,6 @@ public class ProjectService {
         }
         projectEntity.setUserEntity(userEntity);
         return projectRepository.save(projectEntity);
-    }
-
-    // todo 工程的复制功能
-    // 根据Id复制工程
-    @CacheEvict(value = "Project_Cache", allEntries = true)
-    public ProjectEntity copyProjectByUser(String projectId) {
-        ProjectEntity projectArgs = getProjectById(projectId);
-        ProjectEntity projectEntity = new ProjectEntity();
-        BeanUtils.copyProperties(projectArgs, projectEntity, "id", "createTime");
-        projectRepository.save(projectEntity);
-        // 复制设备
-        deviceService.copyDeviceByProject(projectArgs, projectEntity);
-        // 复制组件
-        componentService.copyComponentByProject(projectArgs, projectEntity);
-        return projectEntity;
     }
 
     // 根据Id删除工程
