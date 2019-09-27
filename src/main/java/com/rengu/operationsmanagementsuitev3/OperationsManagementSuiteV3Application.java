eceb26c89b047d96bc0b301e338fcb197c3aec01
esayUI <1016000577@qq.com>
2019-09-27 11:09:12 +0800

    local

--- src/main/java/com/rengu/operationsmanagementsuitev3/OperationsManagementSuiteV3Application.java
+++ src/main/java/com/rengu/operationsmanagementsuitev3/OperationsManagementSuiteV3Application.java
@@ -1,6 +1,6 @@
 package com.rengu.operationsmanagementsuitev3;
 
-import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
